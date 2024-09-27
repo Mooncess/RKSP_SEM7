18a8cd0792a6a07b5342570bfbdde64d2633690a
Anatoly <shulik.2003@mail.ru>
2024-09-27 12:35:17 +0300

    fix pr1, task2

--- src/practice1/Task2.java
+++ src/practice1/Task2.java
@@ -1,40 +1,45 @@
 package practice1;
 
 import java.util.Scanner;
-import java.util.concurrent.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Task2 {
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newFixedThreadPool(5);
+        Scanner scanner = new Scanner(System.in);
 
         while (true) {
+            System.out.print("Введите число (или 'exit' для выхода): ");
+            String userInput = scanner.nextLine();
+
+            if ("exit".equalsIgnoreCase(userInput)) {
+                break;
+            }
+
             try {
-                System.out.print("Введите число (или 'exit' для выхода): ");
-                Scanner scanner = new Scanner(System.in);
-                String userInput = scanner.nextLine();
-
-                if ("exit".equalsIgnoreCase(userInput)) {
-                    break;
-                }
-
                 int number = Integer.parseInt(userInput);
 
-                Future<Integer> future = executorService.submit(() ->
-                        calculateSquare(number));
+                CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> calculateSquare(number), executorService);
 
-                try {
-                    int result = future.get();
-                    System.out.println("\n[ Результат для числа" + number + " : " + result + " ]");
-                } catch (InterruptedException | ExecutionException e) {
-                    System.err.println("Ошибка при выполнении запроса: " +
-                            e.getMessage());
-                }
+                future.thenAccept(result ->
+                                System.out.println("\n[ Результат для числа " + number + " : " + result + " ]"))
+                        .exceptionally(e -> {
+                            System.err.println("Ошибка при выполнении запроса: " + e.getMessage());
+                            return null;
+                        });
+
             } catch (NumberFormatException e) {
                 System.err.println("Неверный формат числа. Пожалуйста, введите целое число.");
             }
         }
 
         executorService.shutdown();
+        scanner.close();
     }
+
     private static int calculateSquare(int number) {
         int delayInSeconds = ThreadLocalRandom.current().nextInt(1, 6);
         try {
